b0b68ca56456086407c1704fbce1b74aa5d85381
Spencer  Krug <spencer@annexushealth.com>
2023-02-01 10:10:10 -0500

    Working example.

--- src/features/todos/TodoList.js
+++ src/features/todos/TodoList.js
@@ -1,14 +1,19 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import TodoListItem from './TodoListItem'
 
+const selectTodos = state => state.todos
+
 const TodoList = () => {
-  const todos = []
+    //SK: useSelector lets your React components read data from the Redux store. 
+    //SK: Also useSelector automatically subscribes to the Redux store for us so we do not need to call store.subscribe to listen for changes.
+    const todos = useSelector(selectTodos)
 
-  const renderedListItems = todos.map((todo) => {
-    return <TodoListItem key={todo.id} todo={todo} />
-  })
+    const renderedListItems = todos.map((todo) => {
+        return <TodoListItem key={todo.id} todo={todo} />
+    })
 
-  return <ul className="todo-list">{renderedListItems}</ul>
+    return <ul className="todo-list">{renderedListItems}</ul>
 }
 
 export default TodoList
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-
 import './api/server'
-
+import App from './App'
+import './index.css'
+import { Provider } from 'react-redux'
 import store from './store'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    // Render a `<Provider>` around the entire `<App>`,
+    // and pass the Redux store to as a prop
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 )
